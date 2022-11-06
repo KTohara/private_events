5a9aa6e0e92135696e7e64af062646108c26491c
kenta <ktohara@gmail.com>
2022-11-06 18:50:14 -0500

    Fixed nav bar and user show views

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,14 +12,21 @@
 #  updated_at  :datetime         not null
 #
 class Event < ApplicationRecord
+  include ActionView::Helpers
   belongs_to :host, foreign_key: :creator_id, class_name: 'User'
   has_many :invitations
   has_many :attendees, through: :invitations
+  
+  scope :past, -> { where("date < ?", Date.today).order(date: :desc) }
+  scope :future, -> { where('date > ?', Date.today).order(:date) }
 
   def formatted_date
     date.strftime("%a. %b %-d %Y")
   end
 
-  scope :past, -> { where('date < ?', Date.today).order(date: :desc) }
-  scope :future, -> { where('date > ?', Date.today).order(:date) }
+  def time_to_event
+    current = Date.today
+    return "Starts in: #{distance_of_time_in_words(current, date)}" if date > current
+    "Event ended: #{time_ago_in_words(date)} ago"
+  end
 end
